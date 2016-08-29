430ef05e24bfc4de067d3707bf5dc55cd814fb1c
Enrico Naletto <developer.nalettoe@yahoo.it>
2016-08-29 09:28:41 +0200

    Another refactoring

--- app/code/local/Alpenite/Blog/controllers/IndexController.php
+++ app/code/local/Alpenite/Blog/controllers/IndexController.php
@@ -2,15 +2,13 @@
 
 class Alpenite_Blog_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {
+        $posts = Mage::getModel('alpenite_blog/post')
+            ->getCollection()
+            ->addAttributeToSelect('*')
+            ->load();
 
-//        $posts = Mage::getModel('alpenite_blog/post')
-//            ->getCollection()
-//            ->addAttributeToSelect('*')
-//            ->load();
-//        foreach ($posts as $post) {
-//            print "{$post->getTitle()}: {$post->getContent()}<br>";
-//        }
         $this->loadLayout();
+        $this->getLayout()->getBlock('post')->setData("posts_collection", $posts);
         $this->renderLayout();
     }
 }
